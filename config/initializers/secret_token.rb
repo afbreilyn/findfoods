98cf9d4d84f5d749aa2310ca819059505fa4871f
aaron foster bresley <afbresley@gmail.com>
2014-04-21 14:38:04 -0400

    Initial Commit. Just generated.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Findfood::Application.config.secret_key_base = 'd5cfdbd9c5b0408b18f92394d5954488054b2c943bb72372c191c79e693ea2bcf1b20a2af3dcac871a9410ac912017383f06920fa5dcb15f6767bdda367999c6'
