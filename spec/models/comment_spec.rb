d9bded78ffed81c97db2ff428dcb146988ac435f
aaron foster bresley <afbresley@gmail.com>
2014-04-23 19:39:54 -0400

    Passes tests for Comment Model.

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -6,17 +6,14 @@ describe Comment do
 		FactoryGirl.build(:comment,
 			body: "man - what great foods!",
 			user_id: 1,
-			restaurant_id: 2,
+			commentable_id: 2,
+			commentable_type: "Restaurant",
 			id: 13)
 	end
 
 	it { should validate_presence_of(:body) }
 	it { should validate_presence_of(:user_id) }
 
-	it { should belong_to(:user) }
-	it { should belong_to(:parent_comment) }
-	it { should belong_to(:restaurant) }
-
-
-
+	it { should belong_to(:commentable) }
+	
 end
