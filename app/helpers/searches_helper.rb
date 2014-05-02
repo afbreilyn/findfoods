58295792be37a1ccb6bcb69c9b1dc62a0ac87215
aaron foster bresley <afbresley@gmail.com>
2014-05-02 13:17:08 -0400

    Fixes bug in searches-helper, and better refactors index into the partial.

--- app/helpers/searches_helper.rb
+++ app/helpers/searches_helper.rb
@@ -2,17 +2,32 @@ module SearchesHelper
   def feature_helper(restaurants)
     result = []
 
-    @restaurants.each do |restaurant|
+    if restaurants
+      restaurants.each do |restaurant|
+        next if restaurant.longitude.nil? || restaurant.latitude.nil?
 
-      next if restaurant.longitude.nil? || restaurant.latitude.nil?
-
+        j_rest = "{'type': 'Feature',
+                    'geometry': {
+                      'type': 'Point',
+                      'coordinates': [ #{restaurant.longitude}, #{restaurant.latitude} ]
+                    },
+                    'properties': {
+                      'title': '#{h(restaurant.name)}',
+                      'marker-color': '#c41200',
+                      'marker-symbol': 'marker',
+                      'marker-size': 'medium'
+                    }
+                  }"
+        result << j_rest
+      end
+    elsif @restaurant
       j_rest = "{'type': 'Feature',
                   'geometry': {
                     'type': 'Point',
-                    'coordinates': [ #{restaurant.longitude}, #{restaurant.latitude} ]
+                    'coordinates': [ #{@restaurant.longitude}, #{@restaurant.latitude} ]
                   },
                   'properties': {
-                    'title': '#{h(restaurant.name)}',
+                    'title': '#{h(@restaurant.name)}',
                     'marker-color': '#c41200',
                     'marker-symbol': 'marker',
                     'marker-size': 'medium'
@@ -20,7 +35,6 @@ module SearchesHelper
                 }"
       result << j_rest
     end
-
     result.join(", ").html_safe
   end
 end
