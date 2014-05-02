60ad19b82b1e3d4f9498cf539d56541717328ead
aaron foster bresley <afbresley@gmail.com>
2014-05-02 03:06:09 -0400

    Upon login, deletion of account or editing account info, redirects you to root_url.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 		if @user.save 
 			sign_in(@user)
-			redirect_to user_url(@user)
+			redirect_to root_url
 		else
 			flash[:errors] = @user.errors.full_messages
 			render :new
