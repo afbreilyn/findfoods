59e65d4b7a98b79fd0633625379bf2c4d5652fed
aaron foster bresley <afbresley@gmail.com>
2014-05-23 14:22:53 -0400

    Changes back thing on session controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,7 +19,7 @@ class SessionsController < ApplicationController
       redirect_to root_url
     else
       flash[:errors] = ["Invalid email or password"]
-      redirect_to root_url
+      render :new
     end
   end
 
